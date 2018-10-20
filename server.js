65ef34fb9fcde6221fe688ab1eac67073a447cad
Shang Zhou Xia <shangxzx@gmail.com>
2018-10-20 05:46:21 -0400

    Updates to personal project using alias

--- server.js
+++ server.js
@@ -1,65 +1,74 @@
 // call the packages we need
-var express = require('express');        // call express
-var app = express();                 // define our app using express
-var bodyParser = require('body-parser');
+var express = require('express'); // call express
+
 var http = require('http');
 var request = require("request");
+var azure = require('azure-storage');
+var multiparty = require('multiparty');
+var formidable = require("formidable");
+var multer = require('multer')
+var MulterAzureStorage = require('multer-azure-storage')
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
+var app = express(); // define our app using express
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080; // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-var SpeechToText = express.Router();
+// var upload = multer({
+//     storage: new MulterAzureStorage({
+//         azureStorageConnectionString: 'DefaultEndpointsProtocol=https;AccountName=cynosure;AccountKey=xxxxxxxxxxxxxxxxxxxxxxxxxx+yA==;EndpointSuffix=core.windows.net',
+//         azureStorageAccessKey: 'xxxxxxxxxxxxxxxxxx',
+//         azureStorageAccount: 'cynosure',
+//         containerName: 'cynosure',
+//         containerSecurity: 'blob'
+//     }),
+//     onError : function(err, next) {
+//         console.log('error', err);
+//         next(err);
+//       }
+// });
+// app.post('/', upload.any(), function(req, res) {
+//     // // get uploaded template file
+//     const payload = req.files;
+//     // Detect if file was sent
+//     if (undefined === payload) {
+//         res.status(400).json({ok:false, err:'No file uploaded'});
+//     }
+//     console.log(payload);
+//     console.log(payload[0].url);
+//     res.send(payload[0].url);
+// });
 
+// app.get('/', function (req, res) {
+//     console.log(req);
+//     console.log(res);
 
+//     var options = {
+//         method: 'POST',
+//         url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
+//         headers:
+//         {
+//             'postman-token': 'c5c887d3-932f-4905-5933-6585bd3781ab',
+//             'cache-control': 'no-cache',
+//             'ocp-apim-subscription-key': 'dd2e13dafe9b4cd79a739e485c6f8a1e',
+//             'content-type': 'application/json'
+//         },
+//         body:
+//         {
+//             documents:
+//                 [{
+//                     language: 'en',
+//                     id: '1',
+//                     text: 'Hello world. This is some input text that I love.'
+//                 }]
+//         },
+//         json: true
+//     };
 
-// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.post('/', function (req, res) {
+//     request(options, function (error, response, body) {
+//         if (error) throw new Error(error);
 
-    var text = req.body;
-    console.log(req.body);
-    var request = require("request");
+//         console.log(body);
+//     });
+// });
 
-    var options = {
-        method: 'POST',
-        url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
-        headers:
-        {
-            'postman-token': '1c1f8bdc-c20c-5033-a384-7f35bc3ea072',
-            'cache-control': 'no-cache',
-            'ocp-apim-subscription-key': 'dd2e13dafe9b4cd79a739e485c6f8a1e',
-            'content-type': 'application/json'
-        },
-        body:
-        {
-            documents:
-                [{
-                    language: 'en',
-                    id: '1',
-                    text: req.body
-                }]
-        },
-        json: true
-    };
-
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-
-        res.json(body);
-    });
-
-});
-
-
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);

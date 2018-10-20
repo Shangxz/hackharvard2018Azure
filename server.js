0bd49ed31cf327001f1bc744ccad3d9593fb6f40
Shang Zhou Xia <shangxzx@gmail.com>
2018-10-20 05:29:15 -0400

    Updates to personal project using alias

--- server.js
+++ server.js
@@ -39,4 +39,36 @@ app.post('/', upload.any(), function(req, res) {
     res.send(payload[0].url);
 });
 
+app.get('/', function (req, res) {
+    console.log(req);
+    console.log(res);
+    var options = {
+        method: 'POST',
+        url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
+        headers:
+        {
+            'postman-token': 'c5c887d3-932f-4905-5933-6585bd3781ab',
+            'cache-control': 'no-cache',
+            'ocp-apim-subscription-key': 'dd2e13dafe9b4cd79a739e485c6f8a1e',
+            'content-type': 'application/json'
+        },
+        body:
+        {
+            documents:
+                [{
+                    language: 'en',
+                    id: '1',
+                    text: 'Hello world. This is some input text that I love.'
+                }]
+        },
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    });
+});
+
 app.listen(port);
